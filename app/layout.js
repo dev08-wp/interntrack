d16a561226d9cf63fc93d35ac221667dd80517c8
Dev <singhdevraj.r6@gmail.com>
2025-12-02 15:40:58 +0530

    Added working login and signup pages

--- app/layout.js
+++ app/layout.js
@@ -1,28 +1,15 @@
-import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata = {
   title: "InternTrack - Track Your Internship Applications",
-  description: "Never lose track of your internship applications. Organize, manage, and land your dream internship.",
+  description:
+    "Never lose track of your internship applications. Organize, manage, and land your dream internship.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="antialiased">
         {children}
       </body>
     </html>
